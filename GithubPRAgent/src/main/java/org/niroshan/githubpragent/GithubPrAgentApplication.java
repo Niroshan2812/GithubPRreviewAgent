906a805c01201ec9b641e674c686eb510acb8372
Niroshan <niroshan9812@gmail.com>
2025-11-10 00:25:35 +0530

    initilize project

---
+++ GithubPRAgent/src/main/java/org/niroshan/githubpragent/GithubPrAgentApplication.java
@@ -0,0 +1,13 @@
+package org.niroshan.githubpragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPrAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPrAgentApplication.class, args);
+    }
+
+}
---
+++ GithubPRAgent/src/test/java/org/niroshan/githubpragent/GithubPrAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niroshan.githubpragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPrAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
