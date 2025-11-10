1e0b3be292e22d17d92aca5859404428766f941d
Niroshan <niroshan9812@gmail.com>
2025-11-10 09:40:22 +0530

    Define structure

--- GithubPRAgent/src/main/java/org/niroshan/githubpragent/Controller/GitHubWebhookController.java
+++ GithubPRAgent/src/main/java/org/niroshan/githubpragent/Controller/GitHubWebhookController.java
@@ -1,4 +1,12 @@
 package org.niroshan.githubpragent.Controller;
 
+import org.niroshan.githubpragent.Service.GitHubReviewSearvice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GitHubWebhookController {
+
+    @Autowired
+    private GitHubReviewSearvice reviewSearvice;
 }
